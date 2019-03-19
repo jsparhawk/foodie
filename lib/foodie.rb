83f78f385527522632379cb2da1891c1b24ed2de
John Williams <john.williams@dvla.gov.uk>
2019-03-19 08:06:08 +0000

    Updated env file to include modules to save this being done in consuming application

--- lib/foodie.rb
+++ lib/foodie.rb
@@ -2,3 +2,7 @@
 require "foodie/version"
 require 'foodie/food'
 require 'foodie/veggie'
+
+
+include Foodie::Food #This includes the modules in the consuming service namespace
+include Foodie::Food_type
